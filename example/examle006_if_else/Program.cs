4ef47a547b2db99eacbe8805ff0edfda478ea056
Dmirtyu <Pianer190@mail.ru>
2022-10-04 14:52:57 +0300

    риветсвтия для маша

---
+++ example/examle006_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя");
+string username = Console.ReadLine();
+
+if (username == "Маша")
+{
+   Console.WriteLine("Ура ура это Маша"); 
+}
+else
+{
+    Console.Write("Привет,  ");
+    Console.WriteLine(username);
+}
--- example/example001/Program.cs
+++ example/example001/Program.cs
@@ -1,2 +1,1 @@
 ﻿Console.WriteLine("2 Hello, World!");
-
